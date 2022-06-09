f238d1a8c909bd5b213d2c05f7b35779cfa13a1c
QUMU00 <41097128+QUMU00@users.noreply.github.com>
2022-06-09 19:36:06 +0800

    Add files via upload

--- mstocirc-master/rpathway/pathways.R
+++ mstocirc-master/rpathway/pathways.R
@@ -71,20 +71,24 @@ genelist
 #go <- enrichGO(genelist,OrgDb =org.At.tair.db, ont = 'ALL', pAdjustMethod = 'BH',pvalueCutoff = 0.5,qvalueCutoff = 0.2,keyType='ENTREZID')
 go <- enrichGO(genelist,OrgDb =spb[dd], ont = 'ALL', pAdjustMethod = 'BH',pvalueCutoff = 0.5,qvalueCutoff = 0.2,keyType='ENTREZID')
 head(go)
+go
 dim(go)
 dim(go[go$ONTOLOGY=='BP',])
 dim(go[go$ONTOLOGY=='CC',])
 dim(go[go$ONTOLOGY=='MF',])
 
-jpeg(file=paste(c(Args[6],'/1.jpg'),sep="",collapse="/"))
+jpeg(file=paste(c(Args[6],'/1.jpg'),sep="",collapse="/"), width=1200, height=1000, units='px',res=72*2)
 #jpeg(file='/home/qumu/1.jpg')
 #jpeg(file,width=1200,height=600)
-barplot(go,showCateogy=20,drop=T)
+
+barplot(go, showCateogy=20)
+    library(ggplot2)
+ggplot(data=go)+geom_bar(aes(x=Description, y=-log10(pvalue),fill=type),stat='identity')+coord_flip()+scale_x_discrete(limits=go$Description)
 dev.off()
-jpeg(file=paste(c(Args[6],'/2.jpg'),sep="",collapse="/"))
+jpeg(file=paste(c(Args[6],'/2.jpg'),sep="",collapse="/"), width=1200, height=1000, units='px',res=72*2)
 dotplot(go,showCategory=20)
 dev.off()
-jpeg(file=paste(c(Args[6],'/3.jpg'),sep="",collapse="/"),width=680,height=480)
+jpeg(file=paste(c(Args[6],'/3.jpg'),sep="",collapse="/"), width=2400, height=2800, units='px',res=72*4)
 #jpeg(file='/home/qumu/3.jpg')
 #go.BP <- enrichGO(genelist,OrgDb=org.Hs.eg.db,ont='BP',pAdjustMethod='BH',pvalueCutoff=0.05,qvalueCutoff = 0.2,keyType='ENTREZID')
 go.BP <- enrichGO(genelist,OrgDb=spb[dd],ont='BP',pAdjustMethod='BH',pvalueCutoff=0.05,qvalueCutoff = 0.2,keyType='ENTREZID')
@@ -99,21 +103,7 @@ kegg_gene <- x[duplicated(x)] }else{kegg_gene <- genelist}
 kegg <- enrichKEGG(kegg_gene,organism=Args[7],keyType='kegg',pvalueCutoff=0.05,pAdjustMethod='BH',minGSSize=10,maxGSSize=500,qvalueCutoff=0.2,use_internal_data=FALSE)
 head(kegg)
 dim(kegg)
-jpeg(file=paste(c(Args[6],'/4.jpg'),sep="",collapse="/"))
+jpeg(file=paste(c(Args[6],'/4.jpg'),sep="",collapse="/"), width=1200, height=1000, units='px',res=72*2)
 #jpeg(file='/home/qumu/4.jpg')
-dotplot(kegg,showCategory=30)
-dev.off()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+dotplot(kegg,showCategory=20)
+dev.off()   
